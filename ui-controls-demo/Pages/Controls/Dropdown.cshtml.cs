27a196df506a3f4e8d444470d529f8aa2bf3a413
dtaylor <5215143+dentaylor@users.noreply.github.com>
2025-05-16 18:08:28 -0500

    Adds UI Demo project

---
+++ ui-controls-demo/Pages/Controls/Dropdown.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace UiControlsDemo.Pages.Controls
+{
+    public class DropdownModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
