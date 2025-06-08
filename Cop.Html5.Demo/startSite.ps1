f13ea790c210c22acf7b072b109dd278cf8ef08f
dtaylor <5215143+dentaylor@users.noreply.github.com>
2025-06-08 15:28:00 -0500

    Rename demo site project

---
+++ Cop.Html5.Demo/startSite.ps1
@@ -0,0 +1,3 @@
+cd C:\Users\denni\Source\repos\dotnet-control-objects\Cop.Html5.Demo
+
+dotnet run
--- Html5Demo/startSite.ps1
+++
@@ -1,3 +0,0 @@
-cd C:\Users\denni\Source\repos\selenium-control-extensions\Html5Demo
-
-dotnet run
